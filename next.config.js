038ec00d14ecb93590074ceab4bbb40596c165d1
Yash Garg <yashygkf@gmail.com>
2023-04-19 01:18:47 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "pbs.twimg.com",
+      "ipfs.infura.io",
+      "lens.infura-ipfs.io",
+      "statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com",
+      "s3-us-west-2.amazonaws.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
